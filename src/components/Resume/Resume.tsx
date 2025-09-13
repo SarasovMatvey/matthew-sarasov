690516f5a885ac3fc0288a4ff46f29526db240be
matvey sarasov <worksarasovmatvej@gmail.com>
2025-09-13 16:01:56 +0600

    reset default styles

---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,12 @@
+import './Resume.module.css'
+
+function Resume() {
+
+  return (
+    <>
+        Hello :)
+    </>
+  )
+}
+
+export default Resume
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './main.css'
+import Resume from './components/Resume/Resume'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Resume />
   </StrictMode>,
 )
